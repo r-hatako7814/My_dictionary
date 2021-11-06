a3952f8ec77102235da55777321549fd6fec6689
r-hatako7814 <ryuichiro.hata@gmail.com>
2021-11-06 17:10:31 +0000

    customerコントローラー

--- app/controllers/customer/customers_controller.rb
+++ app/controllers/customer/customers_controller.rb
@@ -1,18 +1,41 @@
 class Customer::CustomersController < ApplicationController
 
   def show
+    @customer = current_customer
   end
 
   def edit
+    @customer = current_customer
   end
 
   def update
+    @customer = update(customer_params)
+    if @customer.update(customer_params)
+      redirect_to customers_my_page_path
+      flash[:notice_customer] = "会員情報が更新されました。"
+    else
+      @customer = current_customer
+      render :edit
+    end
   end
 
   def unsubscribe
+    @customer = current_customer
   end
 
   def withdraw
+    @customer = current_customer
+    @customer.update(is_deleted: true)
+    reset_session
+    flash[:notice] = "ありがとうございました。またのご利用を心よりお待ちしております。"
+    redirect_to new_customer_session_path
+  end
+  
+  
+  private
+  
+  def customer_params
+    params.require(:customer).permit(:last_name, :first_name, :last_name_kana, :first_name_kana, :email, :customer_image)
   end
 
 end
--- db/seeds.rb
+++ db/seeds.rb
@@ -5,3 +5,12 @@
 #
 #   movies = Movie.create([{ name: 'Star Wars' }, { name: 'Lord of the Rings' }])
 #   Character.create(name: 'Luke', movie: movies.first)
+Customer.create!(
+ email: 'taro@gmail.com',
+ password: '123456',
+ last_name: '山田',
+ first_name: '太郎',
+ customer_image: 'no_image.jpeg',
+ last_name_kana: 'ヤマダ',
+ first_name_kana: 'タロウ'
+ )
