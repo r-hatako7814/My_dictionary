17412ccb1e2c15e9205b27a21bdf0fb4c6e17300
r-hatako7814 <ryuichiro.hata@gmail.com>
2022-01-05 03:59:02 +0000

    production.rb修正

--- config/environments/production.rb
+++ config/environments/production.rb
@@ -91,6 +91,8 @@ Rails.application.configure do
 
   # Do not dump schema after migrations.
   config.active_record.dump_schema_after_migration = false
+
+  config.action_mailer.delivery_method = :smtp
   config.action_mailer.smtp_settings = {
       port: 587,
       address: 'smtp.gmail.com',
